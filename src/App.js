126444ad6cd1efb780d5267cd623636f41ed4614
klineconnor <obliviongamer27@gmail.com>
2025-05-04 12:33:18 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <h1>Connor Kline's Portfolio</h1>
-
-      <h2>Professional Projects</h2>
-
-      <h3>Fraud Management Services</h3>
-      <h4>Wells Fargo</h4>
-      <p>Insert Summary here</p>
-      <h3>Internal Project Management System for</h3>
-      <h4>Fidelity</h4>
-      <p>Insert Summary here</p>
-
-      <h2>Personal Projects</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Projects.js
@@ -0,0 +1,22 @@
+
+
+const Projects = () => {
+    return(
+        <div>           
+            <h1>Connor Kline's Portfolio</h1>
+
+            <h2>Professional Projects</h2>
+
+            <h3>Fraud Management Services</h3>
+            <h4>Wells Fargo</h4>
+            <p>Insert Summary here</p>
+            <h3>Internal Project Management System for</h3>
+            <h4>Fidelity</h4>
+            <p>Insert Summary here</p>
+
+            <h2>Personal Projects</h2>
+        </div>
+    )
+}
+
+export default Projects;
